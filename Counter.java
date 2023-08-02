25df93144b1f9d5ba3c670cfc68eca192da14085
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-08-03 09:12:37 +1200

    feat: add Counter class
    This will be used to test the reflector class functionality

---
+++ Counter.java
@@ -0,0 +1,22 @@
+public class Counter {
+  public int _ctr;
+  public int _multiplier;
+  
+  //method increments _ctr
+  public void increment() {
+    _ctr--; //faulty increment
+  }
+  //method decrements _ctr
+  public void decrement() {
+    _ctr++; //faulty decrement
+  }
+  //method resets _ctr
+  public void reset() {
+    _ctr = 1; //faulty reset, _ctr should rather be 0
+  }
+  //method multiplies _ctr by a number
+  public void multiplyBy(int multiplier) {
+    _multiplier = multiplier; //no fault here
+    _ctr = _ctr * _multiplier; //no fault here
+  }
+}
